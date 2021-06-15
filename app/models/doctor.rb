d7675189a6ca69b7b293b7801eee6d6d4af40271
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-14 23:02:49 -0300

    Fix typo

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,7 +1,7 @@
 class Doctor < ApplicationRecord
   has_many :appointments
 
-  validantes :name, presence: true
+  validates :name, presence: true
   validates :crm, uniqueness: { scope: [:crm_uf] }
   validates :crm, presence: true
   validates :crm_uf, presence: true
