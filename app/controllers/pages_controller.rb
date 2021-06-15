a5a289e17b61a0b29240b6e0352d7c9df2c04fe7
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-15 18:41:29 -0300

    Count appointments and uniq patients

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,7 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @appointments_count = Appointment.all.where("ends_at <= ?", DateTime.now).count
+    @unique_count_patients = Appointment.select('distinct(patient_id)').where("ends_at <= ?", DateTime.now).count
   end
 end
