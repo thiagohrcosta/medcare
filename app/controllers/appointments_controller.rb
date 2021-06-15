9c90fb6e4616d139ce0cfb6ab82af7a08b80b719
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-15 14:15:41 -0300

    Add appointment view

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,37 @@
 class AppointmentsController < ApplicationController
+  def index
+    @appointments = Appointment.all
+  end
+
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+      if @appointment.save
+        redirect_to appointments_path
+      else
+        render :new
+      end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:starts_at, :ends_at, :doctor_id, :patient_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   delete '/patients/:id', to: 'patients#destroy', as: :destroy_patient
 
   resources :appointments
+  delete '/appointments/:id', to: 'appointments#destroy', as: :destroy_appointment
 end
