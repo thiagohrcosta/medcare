56bff5956cb354d1d86bc9ce616c91bd2790d69f
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-15 12:12:31 -0300

    restrict_with_error add on Doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,5 @@
 class Doctor < ApplicationRecord
-  has_many :appointments, dependent: :restrict
+  has_many :appointments, dependent: :restrict_with_error
 
   validates :name, presence: true
   validates :crm, uniqueness: { scope: [:crm_uf] }
