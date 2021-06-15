f62ecc272d07e8db99fafbb70d0b8770c89ea9c3
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-15 12:21:24 -0300

    Add patient on dash, routes and controller actions

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,30 @@
 class PatientsController < ApplicationController
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def patient_params
+  end
 end
