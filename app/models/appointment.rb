2aa3dad0a1c920efd00d7e04e543319060f9dbe6
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-15 14:45:09 -0300

    add Appointment validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,9 @@
 class Appointment < ApplicationRecord
   belongs_to :patient
   belongs_to :doctor
+
+  validates :patient_id, presence: true
+  validates :doctor_id, presence: true
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
 end
