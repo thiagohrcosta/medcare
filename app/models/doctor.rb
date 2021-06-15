a170fd09cfbf0a27e84be147b71b780a2e6b0567
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-15 12:07:33 -0300

    Add restriction on delete

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,5 @@
 class Doctor < ApplicationRecord
-  has_many :appointments
+  has_many :appointments, dependent: :restrict
 
   validates :name, presence: true
   validates :crm, uniqueness: { scope: [:crm_uf] }
