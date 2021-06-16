c7b4f03901e37c917950c33581d2f9fcae787078
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-16 20:53:42 -0300

    Add Filter with Ransack

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,8 @@ class PagesController < ApplicationController
   def dashboard
     @appointments_count = Appointment.all.where("ends_at <= ?", DateTime.now).count
     @unique_count_patients = Appointment.select('distinct(patient_id)').where("ends_at <= ?", DateTime.now).count
+
+    @q = Doctor.ransack(params[:q])
+    @doctor = @q.result(distinct: true)
   end
 end
