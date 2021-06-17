78c7bd8293bec8061856c1368aa6a5af67344f66
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-16 23:29:35 -0300

    Add test to validate CRM numericality

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,6 +3,6 @@ class Doctor < ApplicationRecord
 
   validates :name, presence: true
   validates :crm, uniqueness: { scope: [:crm_uf] }
-  validates :crm, presence: true
+  validates :crm, presence: true, numericality: { only_integer: true }
   validates :crm_uf, presence: true
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
-  context "Doctor Validation" do
+  context 'Doctor Validation' do
   it 'Doctor is valid with valid attributes' do
     doctor = Doctor.new(
       name: 'Derek Shepherd',
@@ -29,20 +29,30 @@ RSpec.describe Doctor, type: :model do
     expect(doctor).to_not be_valid
   end
 
+  it 'Is not valid if CRM != integer' do
+    doctor = Doctor.new(
+      name: 'Derek Shepherd',
+      crm: '12910a',
+      crm_uf: 'MG'
+    )
+    expect(doctor).to_not be_valid
+  end
+
   it 'Is not valid without a CRM_UF' do
     doctor = Doctor.new(
       name: 'Derek Shepherd',
-      crm: "90123",
-      crm_uf: ""
+      crm: '90123',
+      crm_uf: ''
     )
     expect(doctor).to_not be_valid
   end
 end
 
-  # it 'Is not valid with duplicate CRM / UF' do
-
-  # end
   context 'Uniqueness Validation' do
-    it { should validate_uniqueness_of(:crm).scoped_to(:crm_uf)}
+    it { should validate_uniqueness_of(:crm).scoped_to(:crm_uf) }
+  end
+
+  context 'Doctor associations' do
+    it { should have_many(:appointments) }
   end
 end
