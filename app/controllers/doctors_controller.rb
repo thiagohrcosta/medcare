454017af994badbd3cb8208a889b20fb36892949
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-19 11:42:50 -0300

    refactoring counter

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -53,7 +53,7 @@ class DoctorsController < ApplicationController
   end
 
   def set_counter
-    @appointments_count = Appointment.all.where("ends_at <= ?", DateTime.now).count
+    @appointments_count = Appointment.where("ends_at <= ?", DateTime.now).count
     @unique_count_patients = Appointment.select('distinct(patient_id)').where("ends_at <= ?", DateTime.now).count
   end
 end
