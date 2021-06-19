dc2c040eee3ed1a489626a034c5cc29cf0386dd0
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-19 11:45:40 -0300

    Model refactored to be more DRY

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,9 +1,8 @@
 class Patient < ApplicationRecord
   has_many :appointments
 
-  validates :name, presence: true
+  validates :name, :birth_date, presence: true
   validates :cpf, presence: true, uniqueness: true, length: {is: 11}
   validates :cpf, format: { with: /\A\d+\z/, message: "Digite apenas números"}
-  validates :birth_date, presence: true
   validates_cpf_format_of :cpf
 end
